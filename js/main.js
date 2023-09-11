91ff1bd03aa9fdf144230c97ef993272fe275218
25-Otabek <valiboyevo@gmail.com>
2023-09-11 22:01:06 +0500

    add js section

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const elOneInputName = document.querySelector('.one');
+const elTwoInputName = document.querySelector('.two');
+const elThreeInputName = document.querySelector('.three');
+const elFourInputName = document.querySelector('.four');
+const elFiveInputName = document.querySelector('.five');
+const elSixInputName = document.querySelector('.six');
+const elSevenInputName = document.querySelector('.seven');
+const elEightInputName = document.querySelector('.eight');
+const elNineInputName = document.querySelector('.nine');
+const elTenInputName = document.querySelector('.ten');
+
+const elShowButton = document.querySelector('.show');
+
+const elShowResult = document.querySelector('.result');
+
+elShowButton.addEventListener('click', function () {
+  var ismlar = [elOneInputName.value, elTwoInputName.value, elThreeInputName.value, elFourInputName.value, elFiveInputName.value, elSixInputName.value, elSevenInputName.value, elEightInputName.value, elNineInputName.value, elTenInputName.value];
+
+  var randomIndex = Math.floor(Math.random() * ismlar.length);
+
+  var randomIsmlar = ismlar[randomIndex];
+
+  elShowResult.textContent = randomIsmlar.value;
+})
